d81b1c147e4baff3c4f8141b90174869e99876bb
jc-cybrid <juancarlos@cybrid.app>
2022-06-02 18:52:19 -0500

    chore: Add initial project

---
+++ CybridSDK/CybridSDK.h
@@ -0,0 +1,18 @@
+//
+//  CybridSDK.h
+//  CybridSDK
+//
+//  Created by Cybrid on 2/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CybridSDK.
+FOUNDATION_EXPORT double CybridSDKVersionNumber;
+
+//! Project version string for CybridSDK.
+FOUNDATION_EXPORT const unsigned char CybridSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CybridSDK/PublicHeader.h>
+
+
