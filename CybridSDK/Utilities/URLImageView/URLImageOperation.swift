e756320c65081e681392afbfa694ba9d5fd4b697
jc-cybrid <juancarlos@cybrid.app>
2022-06-16 23:41:11 -0500

    feature: add additional interfaces

---
+++ CybridSDK/Utilities/URLImageView/URLImageOperation.swift
@@ -0,0 +1,42 @@
+//
+//  URLImageOperation.swift
+//  CybridSDK
+//
+//  Created by Cybrid on 16/06/22.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - ImageDownloadOperation
+
+protocol URLImageOperation: Operation {
+  var image: UIImage? { get set }
+  var isExecuting: Bool { get }
+  var completionBlock: (() -> Void)? { get set }
+
+  func cancel()
+}
+
+final class ImageDownloadOperation: AsyncOperation, URLImageOperation {
+
+  var image: UIImage?
+
+  private let url: URL
+  private let dataProvider: DataProvider
+
+  init(url: URL, dataProvider: DataProvider) {
+    self.url = url
+    self.dataProvider = dataProvider
+  }
+
+  override func main() {
+    dataProvider.dataTaskWithURL(url) { [weak self] data, _, error in
+      guard let self = self else { return }
+      defer { self.state = .finished }
+      guard error == nil, let data = data else { return }
+      self.image = UIImage(data: data)
+    }
+    .resume()
+  }
+}
