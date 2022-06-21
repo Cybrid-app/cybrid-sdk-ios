254b9e37e58a5101398bcb3e1a6987c99ce8f346
jc-cybrid <juancarlos@cybrid.app>
2022-06-21 10:54:36 -0500

    feature: add test view for Crypto Price List View

---
+++ CybridSDKTestApp/CryptoPriceListTestVC.swift
@@ -0,0 +1,65 @@
+//
+//  CryptoPriceListViewController.swift
+//  CybridSDK
+//
+//  Created by Cybrid on 20/06/22.
+//
+
+import UIKit
+import CybridSDK
+
+public class CryptoPriceListTestVC: UIViewController {
+
+  lazy var tableView = CryptoPriceListView()
+
+  public init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  @available(iOS, deprecated: 10, message: "You should never use this init method.")
+  required init?(coder: NSCoder) {
+    assertionFailure("init(coder:) should never be used")
+    return nil
+  }
+
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+
+    setupViews()
+  }
+
+  private func setupViews() {
+    setupNavBar()
+    setupTabBar()
+    setupMainView()
+    setupTableView()
+  }
+
+  private func setupMainView() {
+    view.backgroundColor = .white
+  }
+
+  private func setupTableView() {
+    tableView.embed(in: self)
+    tableView.startLiveUpdate()
+  }
+
+  private func setupNavBar() {
+    let appearance = UINavigationBarAppearance()
+    appearance.configureWithOpaqueBackground()
+    appearance.backgroundColor = .white
+    navigationController?.navigationBar.standardAppearance = appearance
+    navigationController?.navigationBar.scrollEdgeAppearance = appearance
+  }
+
+  private func setupTabBar() {
+    if #available(iOS 15.0, *) {
+      let appearance = UITabBarAppearance()
+      appearance.configureWithOpaqueBackground()
+      appearance.backgroundColor = .white
+      tabBarController?.tabBar.standardAppearance = appearance
+      tabBarController?.tabBar.scrollEdgeAppearance = appearance
+    }
+  }
+
+}
