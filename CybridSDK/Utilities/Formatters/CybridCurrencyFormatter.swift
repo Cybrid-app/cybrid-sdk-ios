b2b321f0885a24ba49c4a2f0b0b4bae211a0acfa
Juan Carlos Gonzalez <juancarlos@cybrid.app>
2022-07-04 10:57:05 -0500

    feature: add CurrencyFormatter

--- CybridSDK/Utilities/Formatters/CybridCurrencyFormatter.swift
+++ CybridSDK/Utilities/Formatters/CybridCurrencyFormatter.swift
@@ -5,4 +5,31 @@
 //  Created by Cybrid on 4/07/22.
 //
 
+import CybridApiBankSwift
 import Foundation
+
+struct CybridCurrencyFormatter {
+  typealias BigDecimal = Decimal
+
+  static func getExchangeString(from asset1: AssetBankModel, to asset2: AssetBankModel, price: Int) -> String {
+    let divisor = pow(BigDecimal(10), asset2.decimals)
+    let value = BigDecimal(price)
+    let baseUnit = value / divisor
+    let zeroAmount = asset2.symbol + "0"
+    return format(baseUnit: baseUnit, targetAsset: asset2) ?? zeroAmount
+  }
+
+  static func format(baseUnit: BigDecimal, targetAsset: AssetBankModel) -> String? {
+    let formatter = makeNumberFormatter(with: targetAsset)
+    return formatter.string(from: baseUnit as NSNumber)
+  }
+
+  private static func makeNumberFormatter(with asset: AssetBankModel) -> NumberFormatter {
+    let formatter = NumberFormatter()
+    formatter.currencyCode = asset.code
+    formatter.currencySymbol = asset.symbol
+    formatter.numberStyle = .currency
+    formatter.decimalSeparator = "."
+    return formatter
+  }
+}
