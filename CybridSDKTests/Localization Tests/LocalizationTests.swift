3eef01ff2aefa625897f064a73fbde1aefe7a24b
Juan Carlos Gonzalez <juancarlos@cybrid.app>
2022-07-08 01:06:05 -0500

    feature: add tests for Localization

--- CybridSDKTests/Localization Tests/LocalizationTests.swift
+++ CybridSDKTests/Localization Tests/LocalizationTests.swift
@@ -22,19 +22,6 @@ class LocalizationTests: XCTestCase {
     XCTAssertFalse(isEqual)
   }
 
-  func testCybridLocalizer_withEmptyArguments() {
-    // Given
-    let key = CybridLocalizationKey.cryptoPriceList(.headerCurrency)
-    let localizer = CybridLocalizer()
-
-    // When
-    let localizedString = localizer.localize(with: key, arguments: [])
-
-    // Then
-    XCTAssertFalse(localizedString.isEmpty)
-    XCTAssertNotEqual(localizedString, key.stringValue)
-  }
-
   func testCybridLocalizer_withNoParameters() {
     // Given
     let key = CybridLocalizationKey.cryptoPriceList(.headerCurrency)
@@ -46,5 +33,29 @@ class LocalizationTests: XCTestCase {
     // Then
     XCTAssertFalse(localizedString.isEmpty)
     XCTAssertNotEqual(localizedString, key.stringValue)
+  }
+
+  func testCybridLocalizer_inEnglish() {
+    // Given
+    let key = CybridLocalizationKey.cryptoPriceList(.headerCurrency)
+    let localizer = CybridLocalizer(locale: Locale(identifier: "en_US"))
+
+    // When
+    let localizedString = localizer.localize(with: key)
+
+    // Then
+    XCTAssertEqual(localizedString.stringValue, "Currency")
+  }
+
+  func testCybridLocalizer_inFrench() {
+    // Given
+    let key = CybridLocalizationKey.cryptoPriceList(.headerCurrency)
+    let localizer = CybridLocalizer(locale: Locale(identifier: "fr_CA"))
+
+    // When
+    let localizedString = localizer.localize(with: key)
+
+    // Then
+    XCTAssertEqual(localizedString.stringValue, "Devise")
   }
 }
