c9425276c0d1d8aad35cc62f83580c5d61a4c895
Juan Carlos Gonzalez <juancarlos@cybrid.app>
2022-08-11 14:00:37 -0500

    feature: add missing tests to project

---
+++ CybridSDKTests/ViewModel Tests/BuyQuoteViewModelTests.swift
@@ -0,0 +1,14 @@
+//
+//  BuyQuoteViewModelTests.swift
+//  CybridSDKTests
+//
+//  Created by Cybrid on 11/08/22.
+//
+
+import CybridApiBankSwift
+@testable import CybridSDK
+import XCTest
+
+class BuyQuoteViewModelTests: XCTestCase {
+
+}
