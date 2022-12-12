771cded2a0809fd93acfec5c18a30e889f4ad5d3
Erick Sanchez <erick.sanchez.p05@gmail.com>
2022-12-12 12:38:54 -0600

    Finish component

--- CybridSDK/View Layer/BankAcoountsComponent/BankAccountsViewController.swift
+++ CybridSDK/View Layer/BankAcoountsComponent/BankAccountsViewController.swift
@@ -13,7 +13,7 @@ public final class BankAccountsViewcontroller: UIViewController {
 
     public enum BankAccountsViewState { case LOADING, REQUIRED, DONE, ERROR }
 
-    // private var identityVerificationViewModel: IdentityVerificationViewModel!
+    private var bankAccountsViewModel: BankAccountsViewModel!
     private var theme: Theme!
     private var localizer: Localizer!
 
@@ -23,10 +23,10 @@ public final class BankAccountsViewcontroller: UIViewController {
     public init() {
 
         super.init(nibName: nil, bundle: nil)
-        /*self.identityVerificationViewModel = IdentityVerificationViewModel(
+        self.bankAccountsViewModel = BankAccountsViewModel(
             dataProvider: CybridSession.current,
             UIState: self.currentState,
-            logger: Cybrid.logger)*/
+            logger: Cybrid.logger)
         self.theme = Cybrid.theme
         self.localizer = CybridLocalizer()
         self.setupView()
@@ -42,219 +42,6 @@ public final class BankAccountsViewcontroller: UIViewController {
     func setupView() {
 
         view.backgroundColor = .white
-        self.initComponentContent()
-        self.manageCurrentStateUI()
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 6.0) {
-            self.currentState.value = .REQUIRED
-        }
-    }
-}
-
-extension BankAccountsViewcontroller {
-
-    private func initComponentContent() {
-
-        // -- Component Container
-        self.view.addSubview(self.componentContent)
-        self.componentContent.constraint(attribute: .top,
-                                         relatedBy: .equal,
-                                         toItem: self.view,
-                                         attribute: .topMargin,
-                                         constant: 10)
-        self.componentContent.constraint(attribute: .leading,
-                                         relatedBy: .equal,
-                                         toItem: self.view,
-                                         attribute: .leading,
-                                         constant: 10)
-        self.componentContent.constraint(attribute: .trailing,
-                                         relatedBy: .equal,
-                                         toItem: self.view,
-                                         attribute: .trailing,
-                                         constant: -10)
-        self.componentContent.constraint(attribute: .bottom,
-                                         relatedBy: .equal,
-                                         toItem: self.view,
-                                         attribute: .bottomMargin,
-                                         constant: 10)
-    }
-
-    private func manageCurrentStateUI() {
-
-        // -- Await for UI State changes
-        self.currentState.bind { state in
-
-            self.removeSubViewsFromContent()
-            switch state {
-
-            case .LOADING:
-
-                self.KYCView_Loading()
-
-            case .REQUIRED:
-
-                self.KYCView_Required()
-
-            case .DONE:
-
-                print("")
-                // self.KYCView_Verified()
-
-            case .ERROR:
-
-                print("")
-                // self.KYCView_Error()
-            }
-        }
-    }
-
-    private func removeSubViewsFromContent() {
-
-        for view in self.componentContent.subviews {
-            view.removeFromSuperview()
-        }
-    }
-
-    private func KYCView_Loading() {
-
-        let title = UILabel()
-        title.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        title.translatesAutoresizingMaskIntoConstraints = false
-        title.sizeToFit()
-        title.font = UIValues.componentTitleFont
-        title.textColor = UIValues.componentTitleColor
-        title.textAlignment = .center
-        title.text = "Checking Bank Accounts"
-        // title.setLocalizedText(key: UIStrings.loadingText, localizer: localizer)
-
-        self.componentContent.addSubview(title)
-        title.constraint(attribute: .centerY,
-                         relatedBy: .equal,
-                         toItem: self.componentContent,
-                         attribute: .centerY)
-        title.constraint(attribute: .leading,
-                         relatedBy: .equal,
-                         toItem: self.componentContent,
-                         attribute: .leading,
-                         constant: UIValues.componentTitleMargin.left)
-        title.constraint(attribute: .trailing,
-                         relatedBy: .equal,
-                         toItem: self.componentContent,
-                         attribute: .trailing,
-                         constant: -UIValues.componentTitleMargin.right)
-        title.constraint(attribute: .height,
-                         relatedBy: .equal,
-                         toItem: nil,
-                         attribute: .notAnAttribute,
-                         constant: UIValues.componentTitleHeight)
-
-        // -- Spinner
-        let spinner = UIActivityIndicatorView(style: .medium)
-        spinner.addBelow(toItem: title, height: UIValues.loadingSpinnerHeight, margins: UIValues.loadingSpinnerMargin)
-        spinner.color = UIColor.black
-        spinner.startAnimating()
-    }
-
-    private func KYCView_Required() {
-
-        // -- Title
-        self.createStateTitle(
-            stringKey: UIStrings.requiredText,
-            image: UIImage(named: "kyc_required", in: Bundle(for: Self.self), with: nil)!
-        )
-
-        // -- Buttons
-        let done = CYBButton(title: localizer.localize(with: UIStrings.addAccountButtonText)) {
-            self.openPlaid()
-        }
-
-        self.componentContent.addSubview(done)
-        done.constraint(attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: self.componentContent,
-                           attribute: .leading,
-                           constant: UIValues.componentRequiredButtonsMargin.left)
-        done.constraint(attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: self.componentContent,
-                           attribute: .trailing,
-                           constant: UIValues.componentRequiredButtonsMargin.right)
-        done.constraint(attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: self.componentContent,
-                           attribute: .bottomMargin,
-                           constant: UIValues.componentRequiredButtonsMargin.bottom)
-        done.constraint(attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           constant: UIValues.componentRequiredButtonsHeight)
-    }
-
-    private func createStateTitle(stringKey: String, image: UIImage) {
-
-        let title = UILabel()
-        title.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        title.translatesAutoresizingMaskIntoConstraints = false
-        title.sizeToFit()
-        title.font = UIValues.componentTitleFont
-        title.textColor = UIValues.componentTitleColor
-        title.textAlignment = .center
-        title.text = "No banks accounts connected."
-        // title.setLocalizedText(key: stringKey, localizer: localizer)
-
-        self.componentContent.addSubview(title)
-        title.constraint(attribute: .centerX,
-                       relatedBy: .equal,
-                       toItem: self.componentContent,
-                       attribute: .centerX)
-        title.constraint(attribute: .centerY,
-                       relatedBy: .equal,
-                       toItem: self.componentContent,
-                       attribute: .centerY)
-        title.constraint(attribute: .height,
-                         relatedBy: .equal,
-                         toItem: nil,
-                         attribute: .notAnAttribute,
-                         constant: 25)
-
-        let icon = UIImageView(image: image)
-        self.componentContent.addSubview(icon)
-        icon.constraint(attribute: .centerY,
-                       relatedBy: .equal,
-                       toItem: self.componentContent,
-                       attribute: .centerY)
-        icon.constraint(attribute: .trailing,
-                        relatedBy: .equal,
-                        toItem: title,
-                        attribute: .leading,
-                        constant: -15)
-        icon.constraint(attribute: .width,
-                        relatedBy: .equal,
-                        toItem: nil,
-                        attribute: .notAnAttribute,
-                        constant: 25)
-        icon.constraint(attribute: .height,
-                        relatedBy: .equal,
-                        toItem: nil,
-                        attribute: .notAnAttribute,
-                        constant: 25)
-        icon.isHidden = true
-    }
-
-    private func openPlaid() {
-
-        var linkConfiguration = LinkTokenConfiguration(token: "link-sandbox-09703628-aa09-462f-bdca-2014b9aa3d29") { success in
-            print("public-token: \(success.publicToken) metadata: \(success.metadata)")
-        }
-        let result = Plaid.create(linkConfiguration)
-        switch result {
-        case .failure(let error):
-            print("Unable to create Plaid handler due to: \(error)")
-        case .success(let handler):
-            handler.open(presentUsing: .viewController(self))
-            // linkHandler = handler
-        }
     }
 }
 
