6d94239c951865d28f085291ed51dcce4e15a774
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-03-22 10:07:02 -0600

    Migrate from esanchez/Cybrid_Auto_Init

--- CybridSDK/Core/Config/CybridConfig.swift
+++ CybridSDK/Core/Config/CybridConfig.swift
@@ -16,14 +16,23 @@ public final class CybridConfig {
     internal static var shared = CybridConfig()
 
     // MARK: Internal Properties
-    internal var customerGUID: String = ""
     internal var logger: CybridLogger?
     internal var refreshRate: TimeInterval = 5
     internal lazy var session: CybridSession = .current
     internal var theme: Theme = CybridTheme.default
     internal let assetsURL: String = "https://images.cybrid.xyz/sdk/assets/"
-    internal var fiat: FiatConfig = .usd
-    internal var bearer: String = ""
+
+    // MARK: Interal Properties + Private Set
+    internal private(set) var customerGUID: String = ""
+    internal private(set) var bearer: String = ""
+
+    // MARK: Propertis for Auto Init
+    internal private(set) weak var delegate: CybridDelegate?
+    internal private(set) var fiat: AssetBankModel = FiatConfig.usd.defaultAsset
+    internal var dataProvider: (CustomersRepoProvider & BankProvider & AssetsRepoProvider)?
+    internal private(set) var customer: CustomerBankModel?
+    internal private(set) var bank: BankBankModel?
+    internal var assets: [AssetBankModel] = []
 
     // MARK: Private Properties
     private var _preferredLocale: Locale?
@@ -32,22 +41,24 @@ public final class CybridConfig {
     public func setup(bearer: String,
                       customerGUID: String,
                       environment: CybridEnvironment = .sandbox,
-                      fiat: FiatConfig = .cad,
                       locale: Locale? = nil,
                       logger: CybridLogger? = nil,
                       refreshRate: TimeInterval = 5,
-                      theme: Theme? = nil) {
+                      theme: Theme? = nil,
+                      delegate: CybridDelegate) {
 
         self.bearer = bearer
         self.customerGUID = customerGUID
-        self.fiat = fiat
         self.theme = theme ?? CybridTheme.default
         self.refreshRate = refreshRate
         self._preferredLocale = locale
         self.logger = logger
+        self.dataProvider = CybridSession.current
+        self.delegate = delegate
         self.session.setupSession(authToken: self.bearer)
         CybridApiBankSwiftAPI.basePath = environment.basePath
         CodableHelper.jsonDecoder = CybridJSONDecoder()
+        self.autoLoad()
     }
 
     public func startListeners() {
@@ -90,4 +101,95 @@ extension CybridConfig {
         )
         return localeBundlePath != nil
     }
+}
+
+// MARK: CybridConfig + Auto Init
+
+extension CybridConfig {
+
+    public func refreshBearer(bearer: String) {
+        self.bearer = bearer
+    }
+
+    internal func overrideDataProvider(dataProvider: CustomersRepoProvider & BankProvider & AssetsRepoProvider) {
+        self.dataProvider = dataProvider
+    }
+
+    private func autoLoad() {
+
+        // -- 1. Fetch Customer or check if exists
+        self.fetchCustomer { [self] _ in
+
+            // -- 2. Fetch Bank or check if exists
+            self.fetchBank { _ in
+
+                // -- 3. Fetch Assets
+                self.fetchAssets { _ in
+
+                    // -- 4. SDK Ready
+                    self.delegate?.onSDKReady()
+                }
+            }
+        }
+    }
+
+    private func fetchCustomer(_ completion: @escaping (_ ready: Bool) -> Void) {
+
+        if self.customer == nil {
+            self.dataProvider!.getCustomer(customerGuid: self.customerGUID) { [self] customerResponse in
+                switch customerResponse {
+                case .success(let customer):
+                    self.customer = customer
+                    completion(true)
+                case .failure:
+                    self.logger?.log(.component(.accounts(.pricesDataError)))
+                }
+            }
+        } else {
+            completion(true)
+        }
+    }
+
+    private func fetchBank(_ completion: @escaping (_ ready: Bool) -> Void) {
+
+        if self.bank == nil {
+            self.dataProvider!.fetchBank(guid: (self.customer?.bankGuid)!) { [self] bankResponse in
+                switch bankResponse {
+                case .success(let bank):
+                    self.bank = bank
+                    completion(true)
+                case .failure:
+                    self.logger?.log(.component(.accounts(.pricesDataError)))
+                }
+            }
+        } else {
+            completion(true)
+        }
+    }
+
+    private func fetchAssets(_ completion: @escaping (_ ready: Bool) -> Void) {
+
+        if self.assets.isEmpty {
+            self.dataProvider!.fetchAssetsList { [self] assetsResponse in
+                switch assetsResponse {
+                case .success(let assets):
+
+                    let defaultAssetCode = self.bank?.supportedFiatAccountAssets![0] ?? "USD"
+                    self.assets = assets
+                    self.fiat = assets.first(where: { $0.code == defaultAssetCode }) ?? FiatConfig.usd.defaultAsset
+                    completion(true)
+
+                case .failure:
+                    self.logger?.log(.component(.accounts(.pricesDataError)))
+                }
+            }
+        } else {
+            completion(true)
+        }
+    }
+}
+
+// MARK: Protocol
+public protocol CybridDelegate: AnyObject {
+    func onSDKReady()
 }
--- CybridSDKTestApp/LoginController.swift
+++ CybridSDKTestApp/LoginController.swift
@@ -83,22 +83,12 @@ class LoginController: UIViewController {
         Cybrid.setup(bearer: bearer,
                      customerGUID: guidClient,
                      environment: env,
-                     fiat: .usd,
-                     logger: ClientLogger())
-        
-        if loader != nil {
-            DispatchQueue.main.async {
-                self.loader?.dismiss(animated: true)
-            }
-        }
-        
-        DispatchQueue.main.async {
-            self.performSegue(withIdentifier: "goToComponents", sender: self)
-        }
+                     logger: ClientLogger(),
+                     delegate: self)
     }
 }
 
-// # MARK: UITextField Delegation
+// MARK: UITextField Delegation
 extension LoginController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -110,5 +100,22 @@ extension LoginController: UITextFieldDelegate {
             textField.resignFirstResponder();
         }
         return true;
+    }
+}
+
+// MARK: Cybrid Delegation
+extension LoginController: CybridDelegate {
+    
+    func onSDKReady() {
+        
+        if loader != nil {
+            DispatchQueue.main.async {
+                self.loader?.dismiss(animated: true)
+            }
+        }
+        
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "goToComponents", sender: self)
+        }
     }
 }
