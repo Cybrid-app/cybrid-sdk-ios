69791df194e5749e096b9a05bb9f5aae59001a73
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-03-28 14:34:06 -0600

    Complete fix CYBTextField

--- CybridSDK/Utilities/ReusableViews/TextFields/CYBTextField.swift
+++ CybridSDK/Utilities/ReusableViews/TextFields/CYBTextField.swift
@@ -92,144 +92,155 @@ final class CYBTextField: UITextField {
                    constant: Constants.minHeight)
     }
 
-  private func setupIcon() {
-    iconContainer.subviews.forEach { subview in
-      subview.removeFromSuperview()
+    private func setupIcon() {
+
+        iconContainer.subviews.forEach { subview in
+            subview.removeFromSuperview()
+        }
+        switch icon {
+        case .text(let content):
+            setupTextIcon(content: content)
+        case .urlImage(let imageURLString):
+            setupImageIcon(urlString: imageURLString)
+        case .image(let name):
+            setupImage(name: name)
+        case .none:
+            return
+        }
     }
-    switch icon {
-    case .text(let content):
-      setupTextIcon(content: content)
-    case .urlImage(let imageURLString):
-      setupImageIcon(urlString: imageURLString)
-    case .image(let name):
-        setupImage(name: name)
-    case .none:
-      return
+
+    private func setupUnderline() {
+
+        switch style {
+        case .plain:
+            let underline = UIView()
+            underline.backgroundColor = theme.colorTheme.separatorColor
+            underline.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(underline)
+            underline.constraint(attribute: .leading,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .leading)
+            underline.constraint(attribute: .trailing,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .trailing)
+            underline.constraint(attribute: .top,
+                                 relatedBy: .equal,
+                                 toItem: self,
+                                 attribute: .bottom)
+            underline.constraint(attribute: .height,
+                                 relatedBy: .equal,
+                                 toItem: nil,
+                                 attribute: .notAnAttribute,
+                                 constant: Constants.borderWidth)
+        default:
+            return
+        }
     }
-  }
 
-  private func setupUnderline() {
-    switch style {
-    case .plain:
-      let underline = UIView()
-      underline.backgroundColor = theme.colorTheme.separatorColor
-      underline.translatesAutoresizingMaskIntoConstraints = false
-      self.addSubview(underline)
-      underline.constraint(attribute: .leading,
-                           relatedBy: .equal,
-                           toItem: self,
-                           attribute: .leading)
-      underline.constraint(attribute: .trailing,
-                           relatedBy: .equal,
-                           toItem: self,
-                           attribute: .trailing)
-      underline.constraint(attribute: .top,
-                           relatedBy: .equal,
-                           toItem: self,
-                           attribute: .bottom)
-      underline.constraint(attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           constant: Constants.borderWidth)
-    default:
-      return
+    private func setupTextIcon(content: String) {
+
+        let label = UILabel()
+        label.text = content
+        label.textColor = theme.colorTheme.secondaryTextColor
+        label.translatesAutoresizingMaskIntoConstraints = false
+        iconContainer.addSubview(label)
+        let border = UIView()
+        border.backgroundColor = theme.colorTheme.separatorColor
+        border.translatesAutoresizingMaskIntoConstraints = false
+        iconContainer.addSubview(border)
+
+        label.constraintEdges(to: iconContainer)
+        border.constraint(attribute: .leading,
+                          relatedBy: .equal,
+                          toItem: label,
+                          attribute: .trailing,
+                          constant: Constants.Divider.trailing)
+        border.constraint(attribute: .centerY,
+                          relatedBy: .equal,
+                          toItem: label,
+                          attribute: .centerY)
+        border.constraint(attribute: .height,
+                          relatedBy: .equal,
+                          toItem: nil,
+                          attribute: .notAnAttribute,
+                          constant: Constants.Divider.height)
+        border.constraint(attribute: .width,
+                          relatedBy: .equal,
+                          toItem: nil,
+                          attribute: .notAnAttribute,
+                          constant: Constants.borderWidth)
     }
-  }
 
-  private func setupTextIcon(content: String) {
-    let label = UILabel()
-    label.text = content
-    label.textColor = theme.colorTheme.secondaryTextColor
-    label.translatesAutoresizingMaskIntoConstraints = false
-    iconContainer.addSubview(label)
-    let border = UIView()
-    border.backgroundColor = theme.colorTheme.separatorColor
-    border.translatesAutoresizingMaskIntoConstraints = false
-    iconContainer.addSubview(border)
+    private func setupImageIcon(urlString: String) {
 
-    label.constraintEdges(to: iconContainer)
-    border.constraint(attribute: .leading,
-                      relatedBy: .equal,
-                      toItem: label,
-                      attribute: .trailing,
-                      constant: Constants.Divider.trailing)
-    border.constraint(attribute: .centerY,
-                      relatedBy: .equal,
-                      toItem: label,
-                      attribute: .centerY)
-    border.constraint(attribute: .height,
-                      relatedBy: .equal,
-                      toItem: nil,
-                      attribute: .notAnAttribute,
-                      constant: Constants.Divider.height)
-    border.constraint(attribute: .width,
-                      relatedBy: .equal,
-                      toItem: nil,
-                      attribute: .notAnAttribute,
-                      constant: Constants.borderWidth)
-  }
-
-  private func setupImageIcon(urlString: String) {
-    guard let imageView = URLImageView(urlString: urlString) else { return }
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    iconContainer.addSubview(imageView)
-    imageView.constraintEdges(to: iconContainer, insets: Constants.Icon.insets)
-    imageView.constraint(attribute: .height,
-                         relatedBy: .equal,
-                         toItem: nil,
-                         attribute: .notAnAttribute,
-                         constant: Constants.Icon.size.height)
-    imageView.constraint(attribute: .width,
-                         relatedBy: .equal,
-                         toItem: nil,
-                         attribute: .notAnAttribute,
-                         constant: Constants.Icon.size.width)
-  }
+        guard let imageView = URLImageView(urlString: urlString) else { return }
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        iconContainer.addSubview(imageView)
+        imageView.constraintEdges(to: iconContainer, insets: Constants.Icon.insets)
+        imageView.constraint(attribute: .height,
+                             relatedBy: .equal,
+                             toItem: nil,
+                             attribute: .notAnAttribute,
+                             constant: Constants.Icon.size.height)
+        imageView.constraint(attribute: .width,
+                             relatedBy: .equal,
+                             toItem: nil,
+                             attribute: .notAnAttribute,
+                             constant: Constants.Icon.size.width)
+    }
 
     private func setupImage(name: String) {
-        let imageView = UIImageView(image: UIImage(named: name, in: Bundle(for: Self.self), with: nil)!)
-      imageView.translatesAutoresizingMaskIntoConstraints = false
-      iconContainer.addSubview(imageView)
-      imageView.constraintEdges(to: iconContainer, insets: Constants.Icon.insets)
-      imageView.constraint(attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           constant: Constants.Icon.size.height)
-      imageView.constraint(attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           constant: Constants.Icon.size.width)
+
+        let image = UIImage(named: name, in: Bundle(for: Self.self), with: nil)!
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        iconContainer.addSubview(imageView)
+        imageView.constraintEdges(to: iconContainer, insets: Constants.Icon.insets)
+        imageView.constraint(attribute: .height,
+                             relatedBy: .equal,
+                             toItem: nil,
+                             attribute: .notAnAttribute,
+                             constant: Constants.Icon.size.height)
+        imageView.constraint(attribute: .width,
+                             relatedBy: .equal,
+                             toItem: nil,
+                             attribute: .notAnAttribute,
+                             constant: Constants.Icon.size.width)
     }
 }
 
 extension CYBTextField {
-  enum Constants {
-    static let cornerRadius = UIConstants.radiusLg
-    static let insets = UIEdgeInsets(top: 0,
-                                     left: UIConstants.spacingMd,
-                                     bottom: 0,
-                                     right: UIConstants.spacingMd)
-    static let minHeight: CGFloat = UIConstants.minimumTargetSize
-    static let borderWidth: CGFloat = 1
 
-    enum Divider {
-      static let trailing: CGFloat = -7
-      static let height: CGFloat = 22
-    }
+    enum Constants {
+    
+        static let cornerRadius = UIConstants.radiusLg
+        static let insets = UIEdgeInsets(top: 0,
+                                         left: UIConstants.spacingMd,
+                                         bottom: 0,
+                                         right: UIConstants.spacingMd)
+        static let minHeight: CGFloat = UIConstants.minimumTargetSize
+        static let borderWidth: CGFloat = 1
 
-    enum IconContainer {
-      static let minWidth: CGFloat = UIConstants.sizeLg
-    }
+        enum Divider {
 
-    enum Icon {
-      static let insets = UIEdgeInsets(top: 0,
-                                       left: UIConstants.spacingXl2,
-                                       bottom: 0,
-                                       right: UIConstants.spacingMd)
-      static let size = CGSize(width: UIConstants.sizeMd, height: UIConstants.sizeMd)
+            static let trailing: CGFloat = -7
+            static let height: CGFloat = 22
+        }
+
+        enum IconContainer {
+
+            static let minWidth: CGFloat = UIConstants.sizeLg
+        }
+
+        enum Icon {
+
+            static let insets = UIEdgeInsets(top: 0,
+                                             left: UIConstants.spacingXl2,
+                                             bottom: 0,
+                                             right: UIConstants.spacingMd)
+            static let size = CGSize(width: UIConstants.sizeMd, height: UIConstants.sizeMd)
+        }
     }
-  }
 }
