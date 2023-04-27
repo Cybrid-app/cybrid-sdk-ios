e507fa7b2a2b5dd860dc74152d8625f00f41aaf1
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-04-27 10:41:17 -0600

    Fix test part 2

--- CybridSDKTests/Mocks/Prices/PricesAPIMock.swift
+++ CybridSDKTests/Mocks/Prices/PricesAPIMock.swift
@@ -9,22 +9,22 @@ import CybridApiBankSwift
 import CybridSDK
 
 final class PricesAPIMock: PricesAPI {
-  typealias AssetsListCompletion = (_ result: Result<[SymbolPriceBankModel], ErrorResponse>) -> Void
-  private static var listPricesCompletion: AssetsListCompletion?
 
-  @discardableResult
-  override class func listPrices(symbol: String? = nil,
-                                 apiResponseQueue: DispatchQueue = CybridApiBankSwiftAPI.apiResponseQueue,
-                                 completion: @escaping ((Result<[SymbolPriceBankModel], ErrorResponse>) -> Void)) -> RequestTask {
-    listPricesCompletion = completion
-    return listPricesWithRequestBuilder().requestTask
-  }
+    typealias AssetsListCompletion = (_ result: Result<[SymbolPriceBankModel], ErrorResponse>) -> Void
+    private static var listPricesCompletion: AssetsListCompletion?
 
-  class func didFetchPricesSuccessfully(_ prices: [SymbolPriceBankModel]? = nil) {
-    listPricesCompletion?(.success(prices ?? .mockPrices))
-  }
+    override class func listPrices(symbol: String? = nil,
+                                   apiResponseQueue: DispatchQueue = CybridApiBankSwiftAPI.apiResponseQueue,
+                                   completion: @escaping ((Result<[SymbolPriceBankModel], ErrorResponse>) -> Void)) -> RequestTask {
+        listPricesCompletion = completion
+        return listPricesWithRequestBuilder().requestTask
+    }
 
-  class func didFetchPricesWithError() {
-    listPricesCompletion?(.failure(.error(0, nil, nil, CybridError.serviceError)))
-  }
+    class func didFetchPricesSuccessfully(_ prices: [SymbolPriceBankModel]? = nil) {
+        listPricesCompletion?(.success(prices ?? .mockPrices))
+    }
+
+    class func didFetchPricesWithError() {
+        listPricesCompletion?(.failure(.error(0, nil, nil, CybridError.serviceError)))
+    }
 }
