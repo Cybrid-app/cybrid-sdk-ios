b7bd3dbe08639ae660bffb95dc5922ab4f9a7031
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-05-09 09:39:53 -0600

    Initial commit

---
+++ CybridSDKTestAppUITests/Flow/TradeFlow.swift
@@ -0,0 +1,20 @@
+//
+//  TradeFlow.swift
+//  CybridSDKTestAppUITests
+//
+//  Created by Erick Sanchez Perez on 05/05/23.
+//
+//  This flow allow:
+//  - Login
+//  - Add EXternal Account
+//  - Transfer 50 USD
+//  - Buy 25 USD of BTC
+//
+
+import Foundation
+import XCTest
+
+class TradeFlow: XCTestCase {
+    
+    
+}
