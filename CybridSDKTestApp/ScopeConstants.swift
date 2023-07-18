52aa50bbe1626f0c6be859df663019154a334b33
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-07-18 12:59:36 -0600

    Modify scopes

--- CybridSDKTestApp/ScopeConstants.swift
+++ CybridSDKTestApp/ScopeConstants.swift
@@ -10,9 +10,9 @@ import CybridApiIdpSwift
 
 struct ScopeContants {
     
-    internal static let bankTokenScopes = "banks:read banks:write accounts:read accounts:execute customers:read customers:write customers:execute prices:read quotes:execute trades:execute trades:read workflows:execute workflows:read external_bank_accounts:execute external_bank_accounts:read external_bank_accounts:write transfers:read transfers:execute"
-
+    internal static let bankTokenScopes = "banks:read banks:write customers:read customers:write customers:execute"
+    
     internal static let customerTokenScopes: Set<PostCustomerTokenIdpModel.ScopesIdpModel> = [
-        .customersRead, .customersWrite, .accountsRead, .accountsExecute, .pricesRead, .quotesRead, .quotesExecute, .tradesRead, .tradesExecute, .transfersRead, .transfersExecute, .externalBankAccountsRead, .externalBankAccountsWrite, .externalBankAccountsExecute, .workflowsRead, .workflowsExecute
+        .customersRead, .customersWrite, .accountsRead, .accountsExecute, .pricesRead, .quotesRead, .quotesExecute, .tradesRead, .tradesExecute, .transfersRead, .transfersExecute, .externalBankAccountsRead, .externalBankAccountsWrite, .externalBankAccountsExecute, .workflowsRead, .workflowsExecute, .depositAddressesRead, .depositAddressesExecute
     ]
 }
