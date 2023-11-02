3e7cc33b1496bc2f62d3528f7b79517620d11ffc
Erick Sanchez <erick.sanchez.p05@gmail.com>
2023-11-01 23:05:31 -0600

    Add new tests

---
+++ CybridSDKTests/Components Tests/Trade/Entity/TradeTypeTests.swift
@@ -0,0 +1,32 @@
+//
+//  TradeTypeTests.swift
+//  CybridSDKTests
+//
+//  Created by Erick Sanchez on 01/11/23.
+//
+
+import XCTest
+@testable import CybridSDK
+
+class TradeTypeTests: XCTestCase {
+
+    func testLocalizationKeyForBuy() {
+        let tradeType = TradeType.buy
+        XCTAssertEqual(tradeType.localizationKey, CybridLocalizationKey.trade(.buy(.title)))
+    }
+
+    func testLocalizationKeyForSell() {
+        let tradeType = TradeType.sell
+        XCTAssertEqual(tradeType.localizationKey, CybridLocalizationKey.trade(.sell(.title)))
+    }
+
+    func testSideBankModelForBuy() {
+        let tradeType = TradeType.buy
+        XCTAssertEqual(tradeType.sideBankModel, .buy)
+    }
+
+    func testSideBankModelForSell() {
+        let tradeType = TradeType.sell
+        XCTAssertEqual(tradeType.sideBankModel, .sell)
+    }
+}
